4dac2a795add7e180369bb7597e56de3425a215c
junior <juniormathiias@gmail.com>
2023-04-15 21:23:01 -0300

    preparing the test of validation form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import { useState } from 'react';
 import * as S from './styles';
 
 function App() {
+
+  const [form,setForm] = useState({
+    email: {
+      hasChanged: false,
+      value: ""
+    }
+  })
+
+
+
   return (
     <S.Container className='centralize'>
       <S.Form>
-        <S.Input type="email" placeholder="Digite seu email"  data-testeid='email' />
+        <S.Input type="email" placeholder="Digite seu email" value={form.email.value}
+          onChange={e => setForm({...form, email: {
+            hasChanged: true, value: e.target.value}})}
+            data-testeid='email' />
+        {
+          form.email.hasChanged && !form.email.value
+          && <div data-testeid='email-required'> Email é obrigatório</div>
+        }
+
         <S.Input type="password" placeholder="Digite sua senha" />
 
         <S.Button  type="button" className='clear'>Recuperar senha</S.Button >
--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,10 @@ import React from './React';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+
 describe('Login', () => {
+
+  //when is empty
   test('given email, when empty, then show required error message', async () => {
     //lib to render component app
     render(<App />);
@@ -15,6 +18,29 @@ describe('Login', () => {
 
     const requiredError = screen.queryByTestId('email-required');
     expect(requiredError).not.toBeNull();
+  })
+
+  //when is not blank and hide the error message
+  test('given email, when has value, then hide required error message', async () => {
+    //lib to render component app
+    render(<App />);
+
+    //getting id from app.js and checking if the input is blank
+    const email = screen.getByTestId('email');
+
+    await userEvent.type(email, "anyValue");
+
+    const requiredError = screen.queryByTestId('email-required');
+    expect(requiredError).toBeNull();
+  })
+
+  //when the page is refreshed
+  test('given email, when field not changed, then hide required error message', async () => {
+    //lib to render component app
+    render(<App />);
+
+    const requiredError = screen.queryByTestId('email-required');
+    expect(requiredError).toBeNull();
   })
 
 })

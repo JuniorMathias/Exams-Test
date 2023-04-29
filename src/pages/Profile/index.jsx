a3595be3b6a07a684ae05b0420f4fde035d39d4c
junior <juniormathiias@gmail.com>
2023-04-29 00:09:59 -0300

    styling the profile page

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -3,6 +3,7 @@ import * as S from './styles';
 import avatar from '../../assets/avatar.png';
 import { AuthContext } from '../../contexts/auth';
 import { toast } from "react-toastify";
+import NavBar from '../../components/NavBar';
 
 import firebase from '../../services/firebaseConnection';
 
@@ -11,12 +12,18 @@ import { FiUpload } from 'react-icons/fi';
 
 
 export default function Profile(){
-  const { user,signOut, setUser, storageUser } = useContext(AuthContext);
+  const { user,signOut, setUser, storageUser, loadingAuth } = useContext(AuthContext);
 
-  const [name, setName] = useState(user && user.name);
+  const [name, setName] = useState(user && user.nome);
   const [email, setEmail] = useState(user && user.email);
   const [avatarUrl, setAvatarUrl] = useState(user && user.avatarUrl);
   const [imageAvatar, setImageAvatar] = useState(null);
+
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [birth, setBirth] = useState('');
+  const [phone, setPhone] = useState('');
+  const [errors, setErrors] = useState('');
 
 //  preview before upload it
 function handleFile(e){
@@ -47,10 +54,11 @@ async function handleUpload(){
 
   return(
       <>
-        <S.Content className='content'>  
-            <S.Container>
+      <NavBar />
+        <S.Container>
+            <S.Content className='content'>  
               <S.Form onSubmit={handleSave}>
-                <S.LabelAvatar>
+              <S.LabelAvatar>
                   <S.Span>
                     <FiUpload color='#fff' size={25} />
                   </S.Span>
@@ -67,26 +75,74 @@ async function handleUpload(){
                   }
                   </S.AvatarImage>
                 </S.LabelAvatar>
+            <S.Row>
                 <S.Label>Name</S.Label>
-                <S.FormInput
-                    type="text"
-                    value={name}
-                    onChange={ (e) => setName(e.target.value)} 
-                  />
-                <S.Label>Email</S.Label>
-                <S.FormInput
-                    type="text"
-                    value={email}
-                    disabled={true}
-                    style={{ cursor: 'not-allowed'}}
-                  />
-                  <S.Button type='submit'>Save</S.Button>
-              </S.Form>
-            </S.Container>
-            <S.Container>
-                <S.ButtonSignout onClick={ () => signOut() }>Sign Out </S.ButtonSignout>
-            </S.Container>
-        </S.Content>
+            <S.FormInput
+                type="text"
+                value={name}
+                onChange={ (e) => setName(e.target.value)} 
+                />
+            </S.Row>
+            <S.Row>
+            <S.Label style={{width:'50%'}}>Data de Nascimento<i style={{color:'red'}}>*</i></S.Label>
+            <S.Label>Telefone</S.Label>
+            </S.Row>
+          <S.Row>
+            <S.Input
+              type="date"
+              label="Data de Nascimento"
+              value={birth}
+              onChange={ (e) => setBirth(e.target.value)}
+              style={{marginRight:'10px'}}
+            />
+            
+            <S.Input
+              type="number"
+              placeholder="Telefone"
+              value={phone}
+              onChange={ (e) => setPhone(e.target.value)} 
+            />
+          </S.Row>
+          <S.Label>Email<i style={{color:'red'}}>*</i></S.Label>
+          <S.Row>
+          <S.Label>Email</S.Label>
+            <S.FormInput
+                type="text"
+                value={email}
+                disabled={true}
+                style={{ cursor: 'not-allowed'}}
+                />
+          </S.Row>
+          <S.Row>
+            <S.Label style={{width:'50%'}}>Senha<i style={{color:'red'}}>*</i></S.Label>
+            <S.Label>Confirmar senha<i style={{color:'red'}}>*</i></S.Label>
+            </S.Row>
+          <S.Row>
+            <S.Input
+              type="password"
+              placeholder="*******"
+              value={password}
+              autoComplete="new-password"
+              style={{marginRight:'10px'}}
+              onChange={ (e) => setPassword(e.target.value)} 
+            />
+            <S.Input
+              type="password"
+              placeholder="Confirme seu email"
+              value={confirmPassword}
+              onChange={ (e) => setConfirmPassword(e.target.value)} 
+            />
+          </S.Row>
+          
+          <S.labelError>{errors}</S.labelError>
+          <S.Button
+            type="submit"
+          >
+            {loadingAuth ? 'Carregando...' : 'Salvar'}
+          </S.Button>
+        </S.Form>
+            </S.Content>
+        </S.Container>
       </>
   )
 }
--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -1,35 +1,36 @@
 
 import styled from 'styled-components';
 
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 10px;
+  height: 100vh;
+`;
 export const Content = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  // aqui tem que mudar para colocar lado a lado
+  flex-direction: column;
+  width: 100%;
+  box-shadow: 0 1px 2px #0003;
+  background-image: linear-gradient(to right, #2dd36f , #121212);
+  max-width: 750px;
+  padding: 0px 30px;
+  border-radius: 5px;
 `;
 
-export const Container = styled.div`
-    display: flex;
-    background-color: #dcdcdc;
-    border-radius: 5px;
-    padding: .8em;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    margin-top: 1em;
-    margin-left: 10%;
-    @media screen and (max-width: 750px){
-        margin-left: 0;
-        margin-top: 0;
-        margin-bottom: 1em;
-    }
-`;
 export const Form = styled.form`
-    margin-bottom: 1em;
-    border-radius: 50%;
+    display: flex;
+    flex-direction: column;
     object-fit: cover;
-    margin-bottom: 1em;
-    padding: .7em;
-    border:0;
-    @media screen and (max-width: 700px){
-        margin-left: 0;
-    }
+    
+`;
+export const Row = styled.div`
+  display: flex;
 `;
 
 export const LabelAvatar = styled.label`
@@ -40,20 +41,15 @@ export const LabelAvatar = styled.label`
     align-items: center;
     flex-direction: column;
     cursor: pointer;
-    margin-left: 1.5em;
 `;
 export const FormInputFile = styled.input`
     display: none;
-    margin-bottom: 1em;
-    padding: 0.7em;
-    border: 0;
-    border-radius: 5px;
-    max-width: 600px;
-    display: none;
 `;
 export const AvatarImage = styled.div`
-    margin-bottom: 1em;
     border-radius: 50%;
+    margin:auto;
+    -webkit-filter: drop-shadow(2px 3px 6px #fff);
+    filter: drop-shadow(2px 3px 6px #fff);
     object-fit: cover;
 `;
 export const Label = styled.label`
@@ -61,9 +57,7 @@ export const Label = styled.label`
     font-size: 1.4em;
     font-weight: bold;
     display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
+    color: #fff;
 `;
 
 export const Span = styled.span`
@@ -78,12 +72,17 @@ export const Span = styled.span`
 `;
 export const FormInput = styled.input`
     margin-bottom: 1em;
-    margin-top: 0.5em;
     padding: .7em;
-    padding-right: 10rem;
     border:0;
     border-radius: 5px;
-    max-width: 600px;
+`;
+export const Input = styled.input`
+  border: 0;
+  width: 100%;
+  margin-bottom: 12px;
+  height: 36px;
+  border-radius: 4px;
+  padding: 0 8px;
 `;
 export const Button = styled.button`
     padding: 8px 40px 8px 40px;
@@ -96,21 +95,14 @@ export const Button = styled.button`
     align-items: center;
     margin-left: 32%;
 `;
-export const ButtonSignout = styled.button`
-    padding: 8px 40px 8px 40px;
-    background-color: #121212;
-    color: white;
-    border-radius: 5px;
-    font-size: 1.5em;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-left: 1%;
-    &:hover {
-        color: red;
-        transition: ease-in-out .4s;
-    }
-`;
 
+export const labelError = styled.label`
+  font-size: 14px;
+  color: #fff;
+  width: auto;
+  background-color: red;
+  font-weight: bold;
+  margin-bottom: 0.3%;
+`;
 
 

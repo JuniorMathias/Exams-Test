7555ada39a7b2b7e4f7944fb4e7603071d86ab43
junior <juniormathiias@gmail.com>
2023-05-03 23:23:43 -0300

    starting the exams pages

---
+++ src/pages/AdsTest/index.jsx
@@ -0,0 +1,17 @@
+import * as S from './styles';
+
+export default function AdsTest(){
+
+
+
+  return(
+  <>
+  <S.Container>
+  <S.Content>
+    
+    </S.Content>
+
+  </S.Container>
+  </>
+  )
+}
---
+++ src/pages/AdsTest/styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import { Link as Router } from 'react-router-dom';
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 10px;
+  height: 100vh;
+`;
+export const Content = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  box-shadow: 0 1px 2px #0003;
+  background-image: linear-gradient(to right, #2dd36f , #121212);
+  opacity: 0.3px;
+  max-width: 750px;
+  padding: 0px 0px;
+  border-radius: 5px;
+`;

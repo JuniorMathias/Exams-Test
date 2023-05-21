3d2e7adeff5be0e0c4a011fe47b81c7d2928203b
junior <juniormathiias@gmail.com>
2023-05-21 18:42:37 -0300

    starting adjusting some states

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -1,23 +1,28 @@
 import { useContext } from "react";
 import { QuizContext } from "../../contexts/quiz";
 import * as S from './styles';
-import NavBar from '../../components/NavBar';
+import { AuthContext } from '../../contexts/auth';
+
 
 const Welcome = () => {
   const [quizState, dispatch] = useContext(QuizContext);
+  const { logout } = useContext(AuthContext);
+
+  async function handleLogout(){
+    await logout();
+  }
 
 
   return (
-    <>
-    <NavBar />
+    
     <S.Welcome>
+      <S.ButtonLogout onClick={handleLogout}>Sair</S.ButtonLogout>
       <S.H2>Hora de colocar em prática seus conhecimentos</S.H2>
       <S.P>Clique no botão abaixo para começar seu teste:</S.P>
       <S.Button onClick={() => dispatch({ type: "CHANGE_STAGE" })}>
         Iniciar
       </S.Button>
     </S.Welcome>
-    </>
   );
 };
 
--- src/components/Welcome/styles.js
+++ src/components/Welcome/styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
+
 export const Welcome = styled.div`
     text-align: center;
     max-width: 500px;
@@ -15,6 +16,23 @@ export const P = styled.p`
 
 export const H1 = styled.h1`
  
+`;
+export const ButtonLogout = styled.button`
+   position: absolute;
+   bottom: 6%;
+   left: 4%;
+   height: 60px;
+   width: 60px;
+   border-radius: 30px;
+   border: 0;
+   font-weight: bold;
+   background-color: rgba(219, 38, 41, 0.35);
+   color: #fafafa;
+   transition: all 0.5s;
+   &:hover {
+    background-color: rgba(219, 38, 41, 1);
+   color: #fff;
+  }
 `;
 
 export const Button = styled.button`

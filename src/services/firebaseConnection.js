337326499011e30c8db0d41afa07d6b73cdfc50c
junior <juniormathiias@gmail.com>
2023-05-30 23:43:52 -0300

    adding the firebaseConnection

--- src/services/firebaseConnection.js
+++ src/services/firebaseConnection.js
@@ -9,7 +9,13 @@ import { getStorage } from 'firebase/storage';
 
 
 const firebaseConfig = {
-    
+    apiKey: "AIzaSyCMu96MClZ1L8H8C5KpnP3jNkra1Og2IfE",
+    authDomain: "exam-test-7f71d.firebaseapp.com",
+    projectId: "exam-test-7f71d",
+    storageBucket: "exam-test-7f71d.appspot.com",
+    messagingSenderId: "985172525613",
+    appId: "1:985172525613:web:20af89e71b86b0c8282ea4",
+    measurementId: "G-5VJT12W39W"
   };
 
  
